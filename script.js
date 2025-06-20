52d98efa5724753efb3ddd99fbc902b02ea6de34
Zillur-Rahman-Abdullah <zrabdullaho1@gmail.com>
2025-06-21 00:44:08 +0600

    js added

---
+++ script.js
@@ -0,0 +1,18 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function AddTask(){
+    if(inputBox.value===''){
+        alert("Please enter a task");
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML=inputBox.value;
+        listContainer.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value='';
+    saveData();
+}
